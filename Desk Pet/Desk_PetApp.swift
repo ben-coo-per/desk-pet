102f28e0f6d9e43df592007e6b2e06469588e21c
Ben Cooper <cooperben83@gmail.com>
2023-06-24 14:24:38 -0400

    Initial Commit

---
+++ Desk Pet/Desk_PetApp.swift
@@ -0,0 +1,17 @@
+//
+//  Desk_PetApp.swift
+//  Desk Pet
+//
+//  Created by Ben Cooper on 6/24/23.
+//
+
+import SwiftUI
+
+@main
+struct Desk_PetApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
