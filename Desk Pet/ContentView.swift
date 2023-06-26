56dd0ea94003532b593cd6d845f4813d4a91126c
Ben Cooper <cooperben83@gmail.com>
2023-06-26 19:10:48 -0400

    created the first pass at the main view

--- Desk Pet/ContentView.swift
+++ Desk Pet/ContentView.swift
@@ -6,21 +6,41 @@
 //
 
 import SwiftUI
+import ComposableArchitecture
 
 struct ContentView: View {
+    let store: StoreOf<DeskPet>
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        WithViewStore(self.store, observe: { $0 }) { viewStore in
+            VStack {
+                Spacer()
+                ZStack{
+                    Rectangle()
+                        .fill(.gray)
+                        .frame(width: .infinity, height: 50)
+                        .offset(x:0, y:25)
+                    Image("Monkey")
+                        .resizable()
+                        .frame(minWidth: 60, maxWidth: 100, minHeight: 60, maxHeight: 100)
+                        .aspectRatio(1, contentMode: .fit)
+                        // Handles flipping of pet
+                        .rotationEffect(.degrees(180), anchor: .center)
+                        .rotation3DEffect(.degrees(180), axis: (x: 1, y: 0, z:0))
+                        .offset(x:-200, y:-5)
+                }
+            }
+            .frame(minWidth: 300, maxWidth: 500, minHeight: 300, maxHeight: 500)
+            .toolbar {
+                // TODO: This is where the stats will go
+            }
         }
-        .padding()
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView()
+        ContentView(store: Store(initialState: DeskPet.State()) {
+            DeskPet()
+          })
     }
 }
--- Desk Pet/Desk_PetApp.swift
+++ Desk Pet/Desk_PetApp.swift
@@ -6,12 +6,39 @@
 //
 
 import SwiftUI
+import ComposableArchitecture
+
+struct DeskPetPosition: Equatable {
+    var x: Float = 0
+    var direction: Int = 0 // 0 = right, 1 = left
+}
+
+struct DeskPet: ReducerProtocol {
+    struct State: Equatable {
+        var hunger = 0 // hunger on a scale of 0 to 5, 0=fat & happy, 5=dead from starvation
+    }
+    enum Action: Equatable {
+        case feedPet
+      }
+    
+    func reduce(into state: inout State, action: Action) -> EffectTask<Action> {
+        switch action {
+        case .feedPet:
+            if(state.hunger > 0){
+                state.hunger = state.hunger - 1
+            }
+            return .none
+        }
+    }
+}
 
 @main
 struct Desk_PetApp: App {
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            ContentView(store: Store(initialState: DeskPet.State()) {
+                DeskPet()
+              })
         }
     }
 }
